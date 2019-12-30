7bc59692ed90674d7372077167bf4f684b99b47a
Kuznetsov701 <51795621+Kuznetsov701@users.noreply.github.com>
2019-12-30 10:59:17 +0700

    12e

---
+++ Camera.js
@@ -0,0 +1,27 @@
+﻿function runCamera(elementId, constrains = { video: { facingMode: "environment" } }) {
+    try {
+        var video = document.getElementById(elementId);
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia(constrains).then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            });
+        }
+    }
+    catch (err) {
+        alert(err.Message)
+    }
+}
+
+function stopStream(elementId) {
+    try {
+        var video = document.getElementById(elementId);
+        if (video !== undefined) {
+            if (video.srcObject != null) {
+                video.srcObject.getVideoTracks().forEach(track => track.stop());
+            }
+        }
+    } catch (err) {
+        alert(err);
+    }
+}
